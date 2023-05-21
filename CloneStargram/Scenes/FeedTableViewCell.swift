5513fb2998bb009bf7873eeb7ef5cf4a6af93af8
jinan <ja.choi@sweetk.co.kr>
2023-05-22 00:49:36 +0900

    1. 각 버튼 추가

--- CloneStargram/Scenes/FeedTableViewCell.swift
+++ CloneStargram/Scenes/FeedTableViewCell.swift
@@ -10,4 +10,44 @@ import UIKit
 
 final class FeedTableViewCell: UITableViewCell {
     
+    private lazy var postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .tertiaryLabel
+        
+        return imageView
+    }()
+    
+    // MARK: - 좋아요 버튼
+    private lazy var likeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        
+        return button
+    }()
+    
+    // MARK: - 댓글달기 버튼
+    private lazy var commentButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "message"), for: .normal)
+        
+        return button
+    }()
+    
+    // MARK: - dm보내기 버튼
+    private lazy var directMessageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "paperplane"), for: .normal)
+        
+        return button
+    }()
+    
+    // MARK: - 북마크(즐겨찾기) 버튼
+    private lazy var bookmarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "bookmark"), for: .normal)
+        
+        return button
+    }()
+    
+    
 }
