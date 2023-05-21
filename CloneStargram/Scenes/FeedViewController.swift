1bdcc259bef6ef267981e69d3cce156dac8538c3
jinan <ja.choi@sweetk.co.kr>
2023-05-22 00:41:10 +0900

    1. 테이블 뷰를 선언하고 설정 메서드를 추가

--- CloneStargram/Scenes/FeedViewController.swift
+++ CloneStargram/Scenes/FeedViewController.swift
@@ -9,17 +9,42 @@ import SnapKit
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    // 테이블뷰 선언
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none // 컬렉션뷰처럼 활용하기 위해 none으로 설정
+        tableView.dataSource = self // 셀 데이터 설정 추가
+        
+        return tableView
+    }()
 
     
-    
+    // MARK: - 가장 먼저 실행되는 메서드
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupTableView()
     }
     
 
 
+}
+
+extension FeedViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .black
+        
+        return cell
+    }
 }
 
 // MARK: - 메소드만 모을 확장 선언
@@ -38,6 +63,14 @@ private extension FeedViewController {
         )
         
         navigationItem.rightBarButtonItem = uploadButton
+    }
+    
+    // 테이블 뷰 세팅 메서드
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
 }
