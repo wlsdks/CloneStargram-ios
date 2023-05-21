be70640bb32454e00b41e937a2cfe53960faa95f
jinan <ja.choi@sweetk.co.kr>
2023-05-22 01:23:22 +0900

    1. 프로필 뷰 컨트롤러 코드 추가

--- CloneStargram/Scenes/Profile/ProfileViewController.swift
+++ CloneStargram/Scenes/Profile/ProfileViewController.swift
@@ -8,14 +8,145 @@
 import SnapKit
 import UIKit
 
+/**
+ 프로필 탭 컨트롤러
+ */
 final class ProfileViewController: UIViewController {
+    
+    // MARK: - 프로필 이미지 컴포넌트
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 40.0 // 이미지를 둥글게 한다.
+        imageView.layer.borderWidth = 1 // 테두리 두께 설정
+        imageView.layer.borderColor = UIColor.quaternaryLabel.cgColor // 얉은 회색으로 설정
+        
+        return imageView
+    }()
+    
+    // MARK: - 이름 라벨
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "User Name"
+        label.font = .systemFont(ofSize: 13.0, weight: .semibold)
+        
+        return label
+    }()
+    
+    // MARK: - 설명 라벨
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "안녕하세요! 반갑습니다!!"
+        label.font = .systemFont(ofSize: 14.0, weight: .medium)
+        label.numberOfLines = 0 // 라벨의 줄 수가 무한대로 늘어나도 ...이 생기지 않도록 0으로 해준다.
+        
+        return label
+    }()
+    
+    // MARK: - 팔로우와 메시지 버튼 구현
+    private lazy var followButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("팔로우", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14.0, weight: .semibold)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 3.0
+        
+        return button
+    }()
+    
+    private lazy var messageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("메시지", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14.0, weight: .semibold)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 3.0
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.tertiaryLabel.cgColor
+        
+        return button
+    }()
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationItems()
     }
     
     
+}
+
+// MARK: - 확장 선언
+private extension ProfileViewController {
+    
+    // 네비게이션 바 설정 메소드
+    func setupNavigationItems() {
+        navigationItem.title = "Username"
+        
+        let rightBarButton = UIBarButtonItem(
+            image: UIImage(systemName: "ellipsis"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    // 레이아웃 설정
+    func setupLayout() {
+        let buttonStackView = UIStackView(arrangedSubviews: [followButton, messageButton])
+        buttonStackView.spacing = 4.0 // 간격은 4로 설정
+        buttonStackView.distribution = .fillEqually // 두 버튼의 넓이는 동일하게 설정
+        
+        [
+            profileImageView,
+            nameLabel,
+            descriptionLabel
+        ].forEach { view.addSubview($0) }
+        
+        let inset: CGFloat = 16.0
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(inset)
+            $0.leading.equalToSuperview().inset(inset)
+            $0.width.equalTo(80.0)
+            $0.height.equalTo(profileImageView.snp.width)
+        }
+
+//        dataStackView.snp.makeConstraints {
+//            $0.leading.equalTo(profileImageView.snp.trailing).offset(inset)
+//            $0.trailing.equalToSuperview().inset(inset)
+//            $0.centerY.equalTo(profileImageView.snp.centerY)
+//        }
+
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(12.0)
+            $0.leading.equalTo(profileImageView.snp.leading)
+            $0.trailing.equalToSuperview().inset(inset)
+        }
+
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(6.0)
+            $0.leading.equalTo(nameLabel.snp.leading)
+            $0.trailing.equalTo(nameLabel.snp.trailing)
+        }
+
+        buttonStackView.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(12.0)
+            $0.leading.equalTo(nameLabel.snp.leading)
+            $0.trailing.equalTo(nameLabel.snp.trailing)
+        }
+
+//        collectionView.snp.makeConstraints {
+//            $0.leading.equalToSuperview()
+//            $0.trailing.equalToSuperview()
+//            $0.top.equalTo(buttonStackView.snp.bottom).offset(16.0)
+//            $0.bottom.equalToSuperview()
+//        }
+        
+    }
 }
