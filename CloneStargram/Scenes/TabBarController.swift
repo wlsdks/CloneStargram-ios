38533b4a464b7491785b6b4bf5100708c77277a3
jinan <ja.choi@sweetk.co.kr>
2023-05-22 00:25:08 +0900

    1. 탭바 컨트롤러에 하단에 보일 탭바를 2개 생성해줌 -> 일단 이 컨트롤러는 임의로 UIViewController로만 해줌

--- CloneStargram/Scenes/TabBarController.swift
+++ CloneStargram/Scenes/TabBarController.swift
@@ -9,4 +9,24 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feedViewController = UIViewController() // 아직은 안만들어서 임의로 뷰컨트롤러를 설정해줌
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let profileViewController = UIViewController() // 아직은 안만들어서 임의로 뷰컨트롤러를 설정해줌
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        viewControllers = [feedViewController, profileViewController]
+    }
 }
